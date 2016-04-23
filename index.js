92bf32bbc62f25c75c4c93afdefcbe3c99387ab3
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2016-04-23 15:59:32 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+var booljs = require('bool.js');
+
+// Here is where magic happens
+booljs('space.oonowaa.api', ["booljs-mongoose"])
+    .setServerLoader('booljs-express')
+    .setDatabaseLoader('booljs-mongoose')
+    .run();
