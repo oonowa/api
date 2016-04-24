53ef918e09ec0e880b1d494aa38ef6d3fd83eded
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2016-04-24 11:33:29 -0500

    Slight modification

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 var booljs = require('bool.js');
 
 // Here is where magic happens
-booljs('space.oonowaa.api', ["booljs-mongoose"])
+(
+    booljs('space.oonowaa.api', ["booljs-mongoose"])
     .setServerLoader('booljs-express')
     .setDatabaseLoader('booljs-mongoose')
-    .run();
+).run();
